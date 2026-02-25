db38446aa598bc71b17376694beff42041e5037d
dade5950 <david.deriba.5950@student.uu.se>
2026-02-25 14:20:24 +0100

    - Changes to module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,6 @@ module com.example.marstravelsbookings {
     requires javafx.fxml;
 
 
-    opens com.example.marstravelsbookings to javafx.fxml;
-    exports com.example.marstravelsbookings;
-    exports com.example.marstravelsbookings.Main;
     opens com.example.marstravelsbookings.Main to javafx.fxml;
+    exports com.example.marstravelsbookings.Main;
 }
