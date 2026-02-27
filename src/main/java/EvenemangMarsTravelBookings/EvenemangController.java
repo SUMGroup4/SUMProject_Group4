d34764d9d4aafe917a430a1adca70837494d9e20
xepsuper <xepsuper111@gmail.com>
2026-02-27 11:20:56 +0100

    BETA V1.1 - added controller for the button - la till skeleton i koden

--- src/main/java/EvenemangMarsTravelBookings/EvenemangController.java
+++ src/main/java/EvenemangMarsTravelBookings/EvenemangController.java
@@ -1,4 +1,17 @@
 package EvenemangMarsTravelBookings;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class EvenemangController {
+
+    @FXML
+    private Button btnEvenemangVisaFilm;
+
+    @FXML
+    private Button btnEvenemangVisaKonsert;
+
+    @FXML
+    private Button btnEvenemangVisaTeater;
+
 }
