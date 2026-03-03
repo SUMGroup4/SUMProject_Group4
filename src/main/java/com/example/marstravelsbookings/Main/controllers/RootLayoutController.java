b08ddf969bfaa15ae18bd54e75e94262a4aa1717
dade5950 <david.deriba.5950@student.uu.se>
2026-03-03 14:21:41 +0100

    - Implemented new template - Added RootLayout.fxml that seperates views in header, footer, and center areas.

---
+++ src/main/java/com/example/marstravelsbookings/Main/controllers/RootLayoutController.java
@@ -0,0 +1,33 @@
+package com.example.marstravelsbookings.Main.controllers;
+
+import com.example.marstravelsbookings.Main.services.NavigationService;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+
+public class RootLayoutController
+{
+    @FXML
+    private Button bookButton;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private Button cancelButton1;
+
+    @FXML
+    private BorderPane rootPane;
+
+    @FXML
+    private Label selectedPriceLabel;
+    @FXML
+    public void initialize() {
+        // Registrera root
+        NavigationService.setRoot(rootPane);
+
+        // Visa startsida
+        NavigationService.loadView("/views/Evenemang.fxml");
+    }
+}
