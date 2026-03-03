7e67141296d5d6558344f4c9cadc608f36c24892
xepsuper <xepsuper111@gmail.com>
2026-03-03 13:53:35 +0100

    Beta V2 - New controller for a new design - Put a skeleton source code in EvenemangController - Removed other controllers for scalability

--- src/main/java/EvenemangMarsTravelBookings/EvenemangController.java
+++ src/main/java/EvenemangMarsTravelBookings/EvenemangController.java
@@ -1,17 +1,40 @@
 package EvenemangMarsTravelBookings;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 
 public class EvenemangController {
 
     @FXML
-    private Button btnEvenemangVisaFilm;
+    private CheckBox chooseChbxFilmJumanji;
 
     @FXML
-    private Button btnEvenemangVisaKonsert;
+    private CheckBox chooseChbxFilmTerminator;
 
     @FXML
-    private Button btnEvenemangVisaTeater;
+    private CheckBox chooseChbxFilmTitanic;
+
+    @FXML
+    private CheckBox chooseChbxKonsertGwenStefani;
+
+    @FXML
+    private CheckBox chooseChbxKonsertStefanLoven;
+
+    @FXML
+    private CheckBox chooseChbxKonsertTheLeatherNuns;
+
+    @FXML
+    private CheckBox chooseChbxTeaterDracula;
+
+    @FXML
+    private CheckBox chooseChbxTeaterHamlet;
+
+    @FXML
+    private CheckBox chooseChbxTeaterRomeoOchJulia;
+
+    public void checkBoxFunction()
+    {
+
+    }
 
 }
