5c1ee8e574685af25ff9d94fedf5474437f3bc3b
xepsuper <xepsuper111@gmail.com>
2026-03-03 14:35:00 +0100

    Beta V3 - I added TreeMap - Added array checklist - Added if checks on each checkbox

--- src/main/java/EvenemangMarsTravelBookings/EvenemangController.java
+++ src/main/java/EvenemangMarsTravelBookings/EvenemangController.java
@@ -1,10 +1,15 @@
 package EvenemangMarsTravelBookings;
 
+import com.sun.source.tree.Tree;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 
-public class EvenemangController {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.TreeMap;
 
+public class EvenemangController {
+TreeMap<Integer, String> checkIfMovieTrue = new TreeMap<>();
     @FXML
     private CheckBox chooseChbxFilmJumanji;
 
@@ -34,6 +39,51 @@ public class EvenemangController {
 
     public void checkBoxFunction()
     {
+        // filmer
+        boolean[] check = {false,false,false,false,false,false,false,false,false};
+        if(chooseChbxFilmTitanic.isSelected()){
+            check[0] = true;
+
+        } else if (chooseChbxFilmTitanic.isDisabled()) {
+            check[0] = false;
+        }
+
+        if(chooseChbxFilmJumanji.isSelected()){
+            check[1] = true;
+        }
+
+        if(chooseChbxFilmTerminator.isSelected()){
+            check[2] = true;
+        }
+
+        //Konserter
+        if(chooseChbxKonsertGwenStefani.isSelected()){
+            check[3] = true;
+        }
+        if(chooseChbxKonsertStefanLoven.isSelected()){
+            check[4] = true;
+        }
+        if(chooseChbxKonsertTheLeatherNuns.isSelected()){
+            check[5] = true;
+        }
+
+        //Teater
+        if(chooseChbxTeaterDracula.isSelected()){
+            check[6] = true;
+        }
+        if(chooseChbxTeaterHamlet.isSelected()){
+            check[7] = true;
+        }
+        if(chooseChbxTeaterRomeoOchJulia.isSelected()){
+            check[8] = true;
+        }
+
+        for(boolean selection : check){
+            if(selection){
+
+            }
+    }
+
 
     }
 
