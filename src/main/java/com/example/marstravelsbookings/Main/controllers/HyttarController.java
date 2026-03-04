3147c7f961703eacf1aa3ed9ca441d4e6d35d864
dade5950 <david.deriba.5950@student.uu.se>
2026-03-04 12:38:27 +0100

    - Added Hyttar.fxml, HyttarController.java

---
+++ src/main/java/com/example/marstravelsbookings/Main/controllers/HyttarController.java
@@ -0,0 +1,44 @@
+package com.example.marstravelsbookings.Main.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+
+public class HyttarController {
+
+    @FXML
+    private CheckBox choosechbxeconomy1;
+
+    @FXML
+    private CheckBox choosechbxeconomy2;
+
+    @FXML
+    private CheckBox choosechbxeconomy3;
+
+    @FXML
+    private CheckBox choosechbxinside1;
+
+    @FXML
+    private CheckBox choosechbxinside2;
+
+    @FXML
+    private CheckBox choosechbxinside3;
+
+    @FXML
+    private CheckBox choosechbxspaceside1;
+
+    @FXML
+    private CheckBox choosechbxspaceside2;
+
+    @FXML
+    private CheckBox choosechbxspaceside3;
+
+    @FXML
+    private CheckBox choosechbxsvit1;
+
+    @FXML
+    private CheckBox choosechbxsvit2;
+
+    @FXML
+    private CheckBox choosechbxsvit3;
+
+}
